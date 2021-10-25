54109ac3766acfa4af107c6c87319e910e523c5d
GabyLizz <gabylizz.od@gmail.com>
2021-10-25 16:42:07 -0500

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //Creacion de una funcion que muestre el resultado de la suma de dos numeros
+const colors = require('colors');
 
 function suma(numero1, numero2) {
-  console.log(numero1 + numero2);
+  let suma = numero1 + numero2;
+  console.log(`El resultado de la suma es: ${suma}`.red);
 }
 
 suma(10, 20);
